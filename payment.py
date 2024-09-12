730dd91be644d5fc928e2dc5b4b6a8e0fb3ff20a
Charles Walker <cwalke53@me.comcd~ls>
2024-09-11 21:16:39 -0400

    menu

---
+++ payment.py
@@ -0,0 +1,7 @@
+
+def cash_payment(cash, total):
+   balance = cash - total
+   return balance
+def chack_payment(check_amount, total):
+    balance  = check_amount - total
+    return balance
---
+++ product list.py
@@ -0,0 +1,4 @@
+
+
+menu = {"Espresso": 2.50, "Cappuccino": 4.00, "Latte": 4.50, "Americano": 3.00, "Mocha": 5.00, "Cold Brew": 4.25, "Iced Coffee": 3.50, "Matcha Latte": 5.25, "Chai Tea Latte": 4.75,
+        "Bagel": 2.75, "Croissant": 2.50, "Avocado Toast": 5.50, "Chicken Caesar Wrap": 6.50, "Chocolate Chip Cookie": 1.75}
