8a5823d7eaf8a553ceda1ab144d9ea38c6cf966d
Charles Walker <cwalke53@me.comcd~ls>
2024-09-18 16:12:44 -0400

    menu

--- item.py
+++ item.py
@@ -1,36 +1,49 @@
 # Connor Critchley
 # Python Capstone, 9/11/24
+from openpyxl.styles.builtins import total
+
+#from main import quantity
+
 
 class Item:  # Baseline parent Item class
-    def __init__(self, name, price):  # Takes a name and price
+
+    def __init__(self, name, price, quantity):  # Takes a name and price
         self.category = "General"
         self.name = name  # assigns passed in name and price to item instance
         self.price = price
+        self.quantity = quantity
+        self.total = self.quantity * self.price
+
+
 
     def __str__(self):
-        return f"A {self.name} that costs ${self.price:.2f}"
+        return f"{self.quantity} {self.name}(s) that costs ${self.total:.2f}"
 
 
 class Drink(Item):  # Item subclass for drinks
-    def __init__(self, name, price, size):  # Takes in name and price, and size
+    def __init__(self, name, price, size, quantity):  # Takes in name and price, and size
         self.size = size  # assigns drink-specific size variable
-        Item.__init__(self, name, price)  # calls parent constructor for other details
+        Item.__init__(self, name, price, quantity)  # calls parent constructor for other details
         self.category = "Drink"
-        if size == 'L':  # If they ordered a large, increase price by .30
-            self.price += 0.3
+        self.upcharge = 0.30 if size == 'L' else 0
+        self.price += self.upcharge
+        self.total = self.quantity * self.price
 
     def __str__(self):
-        return f"A {'large' if self.size == 'L' else 'regular'} {self.name} that costs ${self.price:.2f}"
+        if self.size == 'L':
+            return f"{self.quantity} large {self.name}(s) (including $0.30 upcharge per item) that costs ${self.total:.2f}"
+        else:
+            return f"{self.quantity} regular {self.name}(s) that costs ${self.total:.2f}"
 
 
 class Food(Item):  # Item subclass for Food
-    def __init__(self, name, price, toasted):  # Takes in name and price, and if toasted
+    def __init__(self, name, price, toasted, quantity):  # Takes in name and price, and if toasted
         self.toasted = toasted  # Assigns food-specific
-        Item.__init__(self, name, price)
+        Item.__init__(self, name, price, quantity)
         self.category = 'Food'
 
     def __str__(self):
-        return f"A {'toasted' if self.toasted else 'plain'} {self.name} that costs ${self.price:.2f}"
+        return f"{self.quantity} {'toasted' if self.toasted else 'plain'} {self.name}(s) that costs ${self.total:.2f}"
 
 if __name__ == '__main__':
     print("Not intended to be run on it's own.")
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 # Plan C
+#from pandas.conftest import join_type
 
 import item as I
 
 # hardcoded menu dictionary
-menu = {"Espresso": 2.50, "Cappuccino": 4.00, "Latte": 4.50, "Americano": 3.00,
-        "Mocha": 5.00, "Cold Brew": 4.25, "Iced Coffee": 3.50, "Matcha Latte": 5.25,
-        "Chai Tea Latte": 4.75, "Bagel": 2.75, "Croissant": 2.50, "Avocado Toast": 5.50,
-        "Chicken Wrap": 6.50, "Club Sandwich": 6.75}
+menu = {"Espresso": 2.49, "Cappuccino": 3.99, "Latte": 4.49, "Americano": 2.99,
+        "Mocha": 4.99, "Cold Brew": 4.29, "Iced Coffee": 3.49, "Matcha Latte": 5.29,
+        "Chai Tea Latte": 4.79, "Bagel": 2.79, "Croissant": 2.49, "Avocado Toast": 5.59,
+        "Chicken Wrap": 6.59, "Club Sandwich": 6.79}
 menu_keys = {0: "Espresso", 1: "Cappuccino", 2: "Latte", 3: "Americano",
              4: "Mocha", 5: "Cold Brew", 6: "Iced Coffee", 7: "Matcha Latte",
              8: "Chai Tea Latte", 9: "Bagel", 10: "Croissant", 11: "Avocado Toast",
@@ -41,10 +42,9 @@ def print_menu():
 
 # main loop
 while running:
-    print("Welcome to StarCreek Coffee, here is our menu:")
+    print("\n------Welcome to StarCreek Coffee-------")
 
     # print out menu
-    print_menu()
     cart = []
     # primary interaction
     ordering = True
@@ -52,10 +52,11 @@ while running:
         choice = ""
         valid = False
         while not valid:
-            choice = input("Make a selection or add to the menu? Type 'select' or 'add'\n")
+            choice = input("Make a selection or add to the menu? Type 'select' to view menu or 'add' to add a new item: \n")
             if choice.upper() in "SELECT" or "ADD":
                 match choice.upper():
                     case 'SELECT':
+                        print_menu()
                         valid = True
                     case 'ADD':
                         add_item()
@@ -64,9 +65,20 @@ while running:
 
         valid = False
         while not valid:
-            choice = int(input("Make a selection:\n")) - 1
+            choice = int(input("Make a selection(Enter item number):\n")) - 1
             if choice in range(len(menu)):
                 valid = True
+
+        # User chooses quantity of item
+        valid_quantity = False
+        quantity = 0
+        while not valid_quantity:
+            try:
+                quantity = int(input("Enter a quantity for item: "))
+                if quantity > 0:
+                    valid_quantity = True
+            except ValueError:
+                print("Enter a valid quantity")
 
         # at this state, we can only have a valid number selection
         # Check if it's in the drink section
@@ -78,39 +90,42 @@ while running:
                 size = input("What's the drink's size? Regular ('R') or Large ('L')? Large is +$0.30\n").upper()
                 if size == 'R' or 'L':
                     valid = True
-            cart.append(I.Drink(menu_keys[choice], menu[menu_keys[choice]], size))
+            cart.append(I.Drink(menu_keys[choice], menu[menu_keys[choice]], size, quantity))
         # Check if in the food section
         elif choice in range(10, 14):
             toasted = True if input("Want it toasted? y/n\n").upper() == 'Y' else False
-            cart.append(I.Food(menu_keys[choice], menu[menu_keys[choice]], toasted))
+            cart.append(I.Food(menu_keys[choice], menu[menu_keys[choice]], toasted, quantity))
         # check for custom item added to menu, anything higher than our preset ones
         elif choice >= 14:
-            cart.append(I.Item(menu_keys[choice], menu[menu_keys[choice]]))
+            cart.append(I.Item(menu_keys[choice], menu[menu_keys[choice]], quantity))
         # Check for exit of ordering loop
         if input('Checkout? y/n\n').upper() == 'Y':
             ordering = False
+        for item in cart:
+            print(item)
 
     # Now out of ordering loop, do payment processing/receipt stuff
     # DO HERE
     tax = 0.06
     subtotal = 0
     for item in cart:
-        subtotal += item.price
+        subtotal += item.price * item.quantity
     grand_total = (subtotal * tax) + subtotal
-    shop_again = True
     print(f"Subtotal: ${subtotal:.2f}\nTax: ${subtotal * tax:.2f}\nGrand Total: ${grand_total:.2f}")
     payment_type = input("How would you like to pay? (Cash/Credit/Check)\n").upper()
     cash = 0
     change = 0
     if payment_type == "CASH":
         cash = float(input("Enter the amount of cash: "))
+        if cash < grand_total:
+            print("You need more cash")
     elif payment_type == "CREDIT":
         card_num = input("Enter your credit card number: ")
         exp_date = input("Enter the expiration date: ")
         Cvv_num = input("Enter the CVV number: ")
     elif payment_type == "CHECK":
         check_num = input("Enter your check number: ")
-    print("Here is your receipt: ")
+    print("------Here is your receipt------- ")
     for item in cart:
         print(item)
     print(f"Payment option: {payment_type}")
@@ -126,4 +141,6 @@ while running:
 
     # Check for exit of main run loop
     if input('\nOrder again? y/n\n').upper() == 'N':
+        print("Thank you for shopping!")
         running = False
+
